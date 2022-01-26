75cb7eac7d594a9a5fb640caba4e4b50a0644ea9
ant1010 <arreola.antony@gmail.com>
2022-01-25 20:05:28 -0700

    aws authentication

--- App.tsx
+++ App.tsx
@@ -4,12 +4,13 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-
+// @ts-ignore
+import { withAuthenticator } from 'aws-amplify-react-native';
 import Amplify from 'aws-amplify'
 import awsconfig from './src/aws-exports'
 Amplify.configure(awsconfig)
 
-export default function App() {
+ function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -24,3 +25,4 @@ export default function App() {
     );
   }
 }
+export default withAuthenticator(App)
